629b1ebd86fcf00f2ebd71bbf95fe816555d54d9
sridhar02 <kattasridhar02@gmail.com>
2020-11-23 15:23:07 +0530

    final application

--- lib/live_view_todos/todos.ex
+++ lib/live_view_todos/todos.ex
@@ -8,24 +8,17 @@ defmodule LiveViewTodos.Todos do
 
   alias LiveViewTodos.Todos.Todo
 
-   @topic inspect(__MODULE__)
-   
-   def subscribde do
-      Phoenix.PubSub.subscribe(LiveViewTodos.PubSub, @topix)
-   end
+  @topic inspect(__MODULE__)
 
-   defp  broadcast_change({:ok, result}, event) do
-     Phoenix.PubSub.broadcast(LiveViewTodos.PubSub, @topic, {__MODULE__, event, result}) 
-   end
+  def subscribe do
+    Phoenix.PubSub.subscribe(LiveViewTodos.PubSub, @topic)
+  end
 
   @doc """
   Returns the list of todos.
-
   ## Examples
-
       iex> list_todos()
       [%Todo{}, ...]
-
   """
   def list_todos do
     Repo.all(Todo)
@@ -33,31 +26,22 @@ defmodule LiveViewTodos.Todos do
 
   @doc """
   Gets a single todo.
-
   Raises `Ecto.NoResultsError` if the Todo does not exist.
-
   ## Examples
-
       iex> get_todo!(123)
       %Todo{}
-
       iex> get_todo!(456)
       ** (Ecto.NoResultsError)
-
   """
   def get_todo!(id), do: Repo.get!(Todo, id)
 
   @doc """
   Creates a todo.
-
   ## Examples
-
       iex> create_todo(%{field: value})
       {:ok, %Todo{}}
-
       iex> create_todo(%{field: bad_value})
       {:error, %Ecto.Changeset{}}
-
   """
   def create_todo(attrs \\ %{}) do
     %Todo{}
@@ -68,15 +52,11 @@ defmodule LiveViewTodos.Todos do
 
   @doc """
   Updates a todo.
-
   ## Examples
-
       iex> update_todo(todo, %{field: new_value})
       {:ok, %Todo{}}
-
       iex> update_todo(todo, %{field: bad_value})
       {:error, %Ecto.Changeset{}}
-
   """
   def update_todo(%Todo{} = todo, attrs) do
     todo
@@ -87,32 +67,31 @@ defmodule LiveViewTodos.Todos do
 
   @doc """
   Deletes a todo.
-
   ## Examples
-
       iex> delete_todo(todo)
       {:ok, %Todo{}}
-
       iex> delete_todo(todo)
       {:error, %Ecto.Changeset{}}
-
   """
   def delete_todo(%Todo{} = todo) do
     todo
     |> Repo.delete()
-    |> broadcast_change([:todo, :deleted])  
+    |> broadcast_change([:todo, :deleted])
   end
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking todo changes.
-
   ## Examples
-
       iex> change_todo(todo)
       %Ecto.Changeset{data: %Todo{}}
-
   """
   def change_todo(%Todo{} = todo, attrs \\ %{}) do
     Todo.changeset(todo, attrs)
+  end
+
+  defp broadcast_change({:ok, result}, event) do
+    Phoenix.PubSub.broadcast(LiveViewTodos.PubSub, @topic, {__MODULE__, event, result})
+
+    {:ok, result}
   end
 end
