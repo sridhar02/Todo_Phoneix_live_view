7ac6ea7e0508771f255c375f09288eb7e900c05d
sridhar02 <kattasridhar02@gmail.com>
2020-11-23 14:25:19 +0530

    init

---
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -0,0 +1,23 @@
+defmodule LiveViewTodosWeb.TodoLive do
+    use LiveViewTodosWeb, :live_view
+    alias LiveViewTodos.Todos
+    
+    def mount(_params,_session,socket) do
+        Todos.subscribe()
+         
+        {:ok, fetch(socket)}    
+    end
+
+    def   handle_event("add", %{"todo" => todo},socket) do
+        Todos.create_todo(todo)
+
+        {:noreply, fetch(socket)}
+    end    
+
+    def handle_info({Todos,[:todo | _], _}, socket) do
+        {:noreply, fetch(socket)}
+    end
+    defp fetch(socket)  do
+        assign(socket,todos: Todos.list_todos()) 
+    end    
+end    
